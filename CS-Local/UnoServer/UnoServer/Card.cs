65ae1e386f62b8cc8242e40169aac389174eb094
Ibon-Eito <71214868+Ibon-Eito@users.noreply.github.com>
2022-04-07 18:37:21 +0200

    Add files via upload

---
+++ CS-Local/UnoServer/UnoServer/Card.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnoServer
+{
+    public class Card
+    {
+        //Todos los campos de los que consta una carta:
+        public int id;
+        public int valor;
+        public string color;
+        public string accion;
+
+        public Card(int Id, int Valor, string Color, string Accion)
+        {
+            id = Id;
+            valor = Valor;
+            color = Color;
+            accion = Accion;
+
+        }
+    }
+}
